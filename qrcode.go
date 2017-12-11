8440c828f7a7a07c93fa0f7660ba9b9d2bcfc804
Dang Minh <supnobita@gmail.com>
2017-12-11 15:52:03 +0700

    Add description to commit

--- qrcode.go
+++ qrcode.go
@@ -8,12 +8,14 @@ import (
 	"io/ioutil"
 )
 
+//main function
 func main() {
 	fmt.Println("Hello QR code ")
 	qrcode := GenerateQRCode("555-2368")
 	ioutil.WriteFile("qrcode.png", qrcode, 0644)
 }
 
+//function gen QR Code to byte array
 func GenerateQRCode(code string) []byte {
 	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
 	buf := new(bytes.Buffer)
